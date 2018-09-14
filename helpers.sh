2b31927f2bf98e07b68e1c0670c70228b8ad33c0
Chris Bode <chris@eonz.net>
2018-09-14 07:49:51 -0400

    created shell helpers file

---
+++ helpers.sh
@@ -0,0 +1,6 @@
+# This is not a script.
+# Use the `source` command to load these variables into the current shell.
+
+eonz_sed_require_style_pattern="s/require [\"]\([^\"]*\)[\"]/require '\1'/g"
+
+alias find_all_sources="find ./src -name \"*.lua\""
